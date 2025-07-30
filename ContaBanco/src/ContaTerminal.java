e69846eb1ac510e24b4b66f86559c8b8c284fbd0
CaioVerkovensk <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>
2025-07-29 22:39:39 -0300

    Implementa a funcionalidade de criação de conta no sistema bancário

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+
+         scanner.nextLine(); // Consome a quebra de linha pendente
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Exibe a mensagem final formatada
+        System.out.println();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
+
+        // Fecha o scanner
+        scanner.close();
     }
 }
